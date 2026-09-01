30ef699661e144859a29deca7514bcc92ccc7fa4
kayyasa Azzahra <kayyasaazzahra@lenovo.local>
2026-09-01 08:25:58 +0700

    adding depedences

--- lib/models/flower.dart
+++ lib/models/flower.dart
@@ -1,4 +1,4 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
 
 import 'package:flutter/material.dart';
 
